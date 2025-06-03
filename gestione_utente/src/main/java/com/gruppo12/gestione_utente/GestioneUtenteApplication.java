f01cbd60e93c5ea64d2d60b64bda615ff923b086
Luis <luisluque18202@gmail.com>
2025-06-03 11:59:10 +0200

    creazione modulo gestione_utente

---
+++ gestione_utente/src/main/java/com/gruppo12/gestione_utente/GestioneUtenteApplication.java
@@ -0,0 +1,13 @@
+package com.gruppo12.gestione_utente;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestioneUtenteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GestioneUtenteApplication.class, args);
+    }
+
+}
---
+++ gestione_utente/src/main/java/com/gruppo12/gestione_utente/Model/UserGestione.java
@@ -0,0 +1,12 @@
+package com.gruppo12.gestione_utente.Model;
+
+public class UserGestione {
+
+    private String id;
+    private String nome;
+    private String cognome;
+    private String email;
+    private String telefono;
+    private String username;
+    private String password;
+}
