62ee7fe6a27478ff2088d450e8c71215539aa9ff
AdrianoPassaro <adrianopassaro235@gmail.com>
2025-06-03 16:54:37 +0200

    new model

---
+++ authentication_autorization/src/main/java/com/gruppo12/authentication_autorization/AuthenticationAutorizationApplication.java
@@ -0,0 +1,16 @@
+package com.gruppo12.authentication_autorization;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+
+@SpringBootApplication
+public class AuthenticationAutorizationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuthenticationAutorizationApplication.class, args);
+    }
+
+}
