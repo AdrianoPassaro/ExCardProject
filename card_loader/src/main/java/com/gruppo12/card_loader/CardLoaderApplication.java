fd6b5bbdbeaf71daaa4d03cd3153f76f0a2f44f2
AdrianoPassaro <adrianopassaro235@gmail.com>
2025-06-03 18:27:08 +0200

    new model

---
+++ card_loader/src/main/java/com/gruppo12/card_loader/CardLoaderApplication.java
@@ -0,0 +1,13 @@
+package com.gruppo12.card_loader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CardLoaderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CardLoaderApplication.class, args);
+	}
+
+}
---
+++ card_loader/src/main/java/com/gruppo12/card_loader/model/LoadRequest.java
@@ -0,0 +1,4 @@
+package com.gruppo12.card_loader.model;
+
+public class LoadRequest {
+}
