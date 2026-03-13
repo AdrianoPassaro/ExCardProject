cf4c65eeb120cc6361f55b98817809cf467106f0
Luis <luisluque18202@gmail.com>
2026-03-13 10:56:58 +0100

    microservizio finito ma senza comunicare con altrimicroservizi

---
+++ collection_tracker/src/test/java/com/gruppo12/collection_tracker/CollectionTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gruppo12.collection_tracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CollectionTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
